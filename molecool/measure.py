86e253701bde9814c3cfcfc1bb2184424190a91b
Tyler Madonna <developer@madonnaapps.com>
2019-11-07 15:56:31 -0500

    Improves variable naming and docstring in calculate_distance function

--- molecool/measure.py
+++ molecool/measure.py
@@ -1,10 +1,31 @@
 import numpy as np
 
-def calculate_distance(rA, rB):
-    # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
-    return dist
+def calculate_distance(point_a, point_b):
+    """
+    This function calculates the distance between two points given as numpy 
+    arrays.
+
+    Parameters
+    ----------
+    point_a, point_b : np.ndarray
+        The coordinates of each point
+    
+    Returns
+    -------
+    distance : float
+        The distance between the two points
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([3.0, 0, 0])
+    >>> calculate_distance(r1, r2)
+    3.0
+    """
+
+    point_difference = point_a - point_b
+    distance = np.linalg.norm(point_difference)
+    return distance
 
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
