3f13f33c97adc08c7d612bd16db7bdfa60669d8f
Tyler Madonna <developer@madonnaapps.com>
2019-11-07 15:05:19 -0500

    Improves package structure of molecool

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,6 +5,9 @@ A python package for analyzing and visualizing molecules for the MolSSI Best Pra
 
 # Add imports here
 from .functions import *
+from .measure import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
 
 # Handle versioneer
 from ._version import get_versions
