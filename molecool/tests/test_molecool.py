ea58dada02955d31eaebdb9eabddd0bf997fcb1e
Tyler Madonna <developer@madonnaapps.com>
2019-11-07 19:11:47 -0500

    Adds unit test for calculate_center_of_mass function

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -1,12 +1,23 @@
 """
-Unit and regression test for the molecool package.
+Unit testing for the molecute package.
 """
 
-# Import package, test suite, and other packages as needed
-import molecool
-import pytest
 import sys
+import numpy as np
+import pytest
+
+import molecool
 
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "molecool" in sys.modules
+
+def test_calculate_center_of_mass():
+    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    coordinates = np.array([[1, 1, 1], [2.4, 1, 1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+
+    calculated_center = molecool.calculate_center_of_mass(symbols, coordinates)
+
+    expected_center = np.array([1, 1, 1])
+
+    assert np.array_equal(expected_center, calculated_center)
